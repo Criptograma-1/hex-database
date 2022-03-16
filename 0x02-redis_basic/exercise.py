8ddd134f716ad3c3a317cab11b4622bd9883f7ab
Criptograma-1 <andresa.info@gmail.com>
2022-03-16 15:19:02 +0000

    Add task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Writing strings to Redis"""
+"""Redis"""
 
 from typing import Union
 import redis
@@ -25,3 +25,36 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None)
+            -> Union[str, bytes, int, float]:
+        """Convert the data back to the desired format
+            Args:
+                key: generated key
+                fn:  Callable used to convert the data
+            Return:
+                Converted data
+        """
+        data = self._redis.get(key)
+        
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Convert data to str"""
+        data = self._redis.get(key)
+
+        return data.decode("utf-8")
+
+     def get_int(self, key: str) -> int:
+         """Convert data to int"""
+         data = self._redis.get(key)
+
+         try:
+             data = int(value.decode("utf-8"))
+         except Exception:
+             data = 0
+         
+         return data
