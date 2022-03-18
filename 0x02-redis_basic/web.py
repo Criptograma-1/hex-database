0c95007ce9eecea3e804a70c750f493469f5f229
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-03-18 14:38:51 -0300

    Change logic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,6 +5,7 @@ import redis
 import requests
 from typing import Callable
 from functools import wraps
+from datetime import timedelta
 
 re = redis.Redis()
 
@@ -13,19 +14,10 @@ def count_requests(method: Callable) -> Callable:
     """ Counting with decorators how many times a request
         has been made
     """
-
     @wraps(method)
-    def wrapper(url):
-        """ Wrapper for decorator functionality """
-        rd.incr(f"count:{url}")
-        cached_html = rd.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-
-        html = method(url)
-        rd.setex(f"cached:{url}", 10)
-        return html
-
+    def wrapper(*args, **kwds):
+        re.incr("count:{}".format(args[0]), 1)
+        return method(*args, **kwds)
     return wrapper
 
 
@@ -34,5 +26,8 @@ def get_page(url: str) -> str:
     """ requests module to obtain the HTML
         content of a particular URL and returns it.
     """
-    req = requests.get(url)
-    return req.text
+   if not re.exists(url):
+        response = requests.get(url)
+        re.setex(url, timedelta(seconds=10), response.content)
+        return response.content
+    return re.get(url)
