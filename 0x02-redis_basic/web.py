f3f83c9d192399a54fc670be4d9d2dffbb505f8a
Criptograma-1 <andresa.info@gmail.com>
2022-03-18 18:27:30 +0000

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
-
-from typing import Callable
+"""Implementing an expiring web cache"""
+import requests
 import redis
 from functools import wraps
-import requests
-from datetime import timedelta
+
+store = redis.Redis()
 
 
-cache = redis.Redis()
-
-
-def count_requests(method: Callable) -> Callable:
-    """Decorator that count number of requests"""
+def count_url_access(method):
+    """ Decorator counting how many times
+    a Url is accessed """
     @wraps(method)
-    def wrapper(*args, **kwds):
-        cache.incr("count:{}".format(args[0]), 1)
-        return method(*args, **kwds)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+        
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@count_requests
+@count_url_access
 def get_page(url: str) -> str:
-    """Obtain the HTML content of a particular URL and returns it."""
-    if not cache.exists(url):
-        res = requests.get(url)
-        cache.setex(url, timedelta(seconds=10), res.content)
-        return res.content
-    return cache.get(url)
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
