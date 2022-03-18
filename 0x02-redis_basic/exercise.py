10fe6aaede7b8321be82ff7cbd1739a78b4bce81
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-03-18 14:06:57 -0300

    Fix  Pycodestyle

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,8 +20,12 @@ def count_calls(method: Callable) -> Callable:
 
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
-    """Method to store the history of inputs and outputs for a particular function"""
+    """
+    Method to store the history of inputs and outputs for 
+    a particular function
+    """
     key = method.__qualname__
     inputs = key + ":inputs"
     outputs = key + ":outputs"
@@ -36,6 +40,7 @@ def call_history(method: Callable) -> Callable:
         return a
 
     return wrapper
+
 
 def replay(method: Callable):
     """Method to display the history of calls of a particular function"""
@@ -62,7 +67,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
@@ -87,7 +91,7 @@ class Cache:
                 Converted data
         """
         data = self._redis.get(key)
-        
+
         if fn:
             return fn(data)
         return data
@@ -106,5 +110,5 @@ class Cache:
             data = int(value.decode("utf-8"))
         except Exception:
             data = 0
-         
+
         return data
