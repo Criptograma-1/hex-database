881668414d78ab8ce2305bdbdb4e2f6f8a5b34c9
Criptograma-1 <andresa.info@gmail.com>
2022-03-18 17:12:31 +0000

    Add task 5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
+
+import redis
+import requests
+
+redis = redis.Redis()
+count = 0
+
+
+def get_page(url: str) -> str:
+    """
+    Method to track how many times a particular URL was accessed
+    and cache the result with an expiration time of 10 seconds.
+    """
+    redis.set(f"cached:{url}", count)
+    response = requests.get(url)
+    redis.incr(f"count:{url}")
+    redis.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+
+    return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
