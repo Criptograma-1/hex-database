14a9b0d2022e8afde780efd131735e6274a7a3b8
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-05-05 18:33:53 -0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,7 +9,7 @@ count = 0
 
 
 def get_page(url: str) -> str:
-    """ get a page and cach value"""
+    """ get a page and cache value"""
     rc.set(f"cached:{url}", count)
     resp = requests.get(url)
     rc.incr(f"count:{url}")
