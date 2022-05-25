644ab328d2f2a2635c0f6ade0922671acf23ca36
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-05-25 16:57:34 -0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-'''
-method that implements an expiring web cache and tracker
-'''
 
 from typing import Callable
 from functools import wraps
@@ -11,13 +8,10 @@ import requests
 
 
 def requests_counter(method: Callable) -> Callable:
-    """ Counts how many times a request has been made
-    """
     r = redis.Redis()
 
     @wraps(method)
     def wrapper(url):
-        """ wrapper fxn that counts actual no of requests made"""
         r.incr(f"count:{url}")
         cached_ = r.get(f"cached:{url}")
         if cached:
@@ -31,7 +25,5 @@ def requests_counter(method: Callable) -> Callable:
 
 
 def get_page(url: str) -> str:
-    """ obtains html content for a given site url and returns it.
-    """
     resp = requests.get(url)
     return resp.text
