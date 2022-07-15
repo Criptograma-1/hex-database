15183d5be883f0f87570fe7c074a557aea8408ca
Criptograma-1 <andresa.info@gmail.com>
2022-07-15 07:05:47 -0700

    [FIX] permission

--- 0x04-NoSQL/102-log_stats.py
+++ 0x04-NoSQL/102-log_stats.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python3
-"""Defines a function that  provides some stats
-   about Nginx logs stored in MongoDB
+"""
+Python script that provides some stats about
+Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
 
-def nginx_stats_check():
-    """
-    provides some stats about Nginx
-    """
-    client = MongoClient()
-    nginx_logs = client.logs.nginx
-    docs = nginx_logs.count_documents({})
-    print("{} logs".format(docs))
-    print("Methods:")
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        method_count = nginx_logs.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, method_count))
-    status = nginx_logs.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status))
+def logs(nginx):
+    '''
+    Prints Nginx request logs.
+    '''
+    print(f'{nginx.count_documents({})} logs')
+    print('Methods:')
+    reqs = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for req in reqs:
+        print(f'\tmethod {req}: {nginx.count_documents({"method": req})}')
+    print(f'{nginx.count_documents({"path": "/status"})} status check')
+
+
+def ips(nginx):
+    '''Prints he most present IPs in the collection'''
     print("IPs:")
-    top_IPs = nginx_logs.aggregate([{"$group":
-        {"_id": "$ip",
-        "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10},
-        {"$project":
-            {"_id": 0, "ip": "$_id", "count": 1}
-        }])
-    for top_ip in top_IPs:
-        count = top_ip.get("count")
-        ip_address = top_ip.get("ip")
-        print("\t{}: {}".format(ip_address, count))
+    lists = nginx.aggregate([{'$match': {}}, {'$group': {
+        '_id': '$ip', 'tot': {'$sum': 1}}},
+        {'$sort': {'tot': -1}}, {'$limit': 10}])
+    for li in lists:
+        print(f'\t{li.get("_id")}: {li.get("tot")}')
 
 
-if __name__ == "__main__":
-    nginx_logs_stats()
+def conn():
+    '''
+    Establish a connection with MongoDB.
+    '''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+    logs(nginx)
+    ips(nginx)
+
+
+if __name__ == '__main__':
+    conn()
